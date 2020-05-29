19012a9c188f86ef290b4c1a7dedcb9e127f3c67
sreskala <sam.sagas@hotmail.com>
2020-05-29 15:14:38 -0400

    Added an icon to the application

--- client/src/components/pages/About.jsx
+++ client/src/components/pages/About.jsx
@@ -13,6 +13,8 @@ const About = () => {
             <h3>Built with Node and Express on the backend, MongoDB for DB handling of users and contacts. React on the front-end. </h3>
             <h3>Used bcrypt hashing with JSONWebToken for authentication.</h3>
             <p style={{textAlign:"center"}}>Sam Reskala {year}&copy; </p>
+
+            <a style={{display:"block", textAlign:"center", fontSize:"10px"}} href="https://icons8.com/icon/47706/businessman">Businessman icon by Icons8</a>
         </div>
     )
 }
